be0d63bb922c21294d70e94e69ac11886bd51be9
gaelL <contact@gaell.info>
2019-02-18 13:34:03 +0100

    terraform: cleanup of amazon ENI on instance termination

--- terraform/module-external-worker/launch_template_spot_c3.tf
+++ terraform/module-external-worker/launch_template_spot_c3.tf
@@ -19,6 +19,7 @@ resource "aws_launch_template" "worker_c3" {
 
   network_interfaces {
     associate_public_ip_address = "${var.worker_associate_public_ip_address}"
+    delete_on_termination = true
 
     security_groups = ["${compact(list(
         "${var.bastion_sg_allow}",
